1baf0ccc751be3080e1da66371af3e79a512e896
Holy-uchi <ax4koji@gmail.com>
2026-02-05 10:15:20 +0900

    第4章の復習問題

---
+++ Chap05/ch05-00.c
@@ -0,0 +1,17 @@
+// 第２問
+// #include <stdio.h>
+
+// int main(int argc, char *argv[])
+// {
+//   if (argc == 1) {
+//     puts("Hello world!");
+//   } else {
+//     puts(argv[1]);
+//   }
+// }
+
+// 第３問
+int main(int argc, char *argv[])
+{
+  return (argc > 1) ? 0 : 1;
+}
